45f8e786c648b35d184df1a57bfcd95798a3f8ae
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-05-01 23:51:30 -0700

    Fetch all GitHub and GitLab repos for a user and put them in tidy de-duped arrays.
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
 const fetch = require('node-fetch')
-const { token, user } = require('./.SECRET')
+const { gitHubToken, gitLabToken, user } = require('./.SECRET')
 
-fetch(`https://api.github.com/users/${user}/repos`)
-    .then((response) => response.json())
-    .then((json) => {
-        console.log(json)
-        process.exit()
-        return false
-        json.map((project) => project.id).forEach((projectId) => {
-            fetch(
-                `https://gitlab.com/api/v4/projects/${projectId}/?private_token=${token}`,
-                {
-                    method: 'POST'
-                }
-            )
-                .then((res) => res.json())
-                .then((json) => {
-                    console.log(json)
-                })
-                .catch((err) => console.error(err))
-        })
+const flatten = (a) => a.reduce((x, y) => x.concat(y))
+
+let gitHubRepos = []
+const getGitHubRepos = (count) =>
+    fetch(`https://api.github.com/users/${user}/repos?type=all&page=${count}`, {
+        headers: {
+            Authorization: `token ${gitHubToken}`
+        }
     })
-    .catch((err) => console.error(err))
+        .then((res) => res.json())
+        .then((setOfGitHubRepos) => {
+            if (setOfGitHubRepos.length) {
+                console.log(`Page ${count} complete.`)
+                gitHubRepos.push(
+                    setOfGitHubRepos.map((repo) =>
+                        repo.name.replace(/[^A-Za-z0-9]/g, '')
+                    )
+                )
+                getGitHubRepos(++count)
+            } else {
+                console.log([...new Set(flatten(gitHubRepos))])
+            }
+        })
+        .catch((err) => console.error(err))
+getGitHubRepos(0)
+
+let gitLabProjects = []
+const getGitLabProjects = (count) =>
+    fetch(`https://gitlab.com/api/v4/users/${user}/projects?page=${count}`)
+        .then((res) => res.json())
+        .then((setOfGitLabProjects) => {
+            if (setOfGitLabProjects.length) {
+                console.log(`Page ${count} complete.`)
+                gitLabProjects.push(
+                    setOfGitLabProjects.map((repo) =>
+                        repo.name.replace(/[^A-Za-z0-9]/g, '')
+                    )
+                )
+
+                getGitLabProjects(++count)
+            } else {
+                console.log([...new Set(flatten(gitLabProjects))])
+            }
+        })
+        .catch((err) => console.error(err))
+getGitLabProjects(0)
+
+//`https://gitlab.com/api/v4/projects/${projectId}/?private_token=${gitLabToken}`,
