5affe5bc1111e23ec20866e61763cb0a7e9b9798
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-05-02 18:09:59 -0700

    It works!
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

---
+++ DELETE_ALL_GITLAB_REPOS.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch')
+const { gitLabToken, user } = require('./.SECRETS')
+const gitLabApi = 'https://gitlab.com/api/v4'
+
+fetch(`${gitLabApi}/users/${user}/projects?private_token=${gitLabToken}`)
+    .then((res) => res.json())
+    .then((json) => {
+        console.log(json)
+        json.map((project) => project.id).forEach((projectId) => {
+            fetch(
+                `${gitLabApi}/projects/${projectId}?private_token=${gitLabToken}`,
+                {
+                    method: 'DELETE'
+                }
+            )
+                .then((res) => res.json())
+                .then((json) => console.log(json))
+                .catch((err) => console.error(err))
+        })
+    })
+    .catch((err) => console.error(err))
---
+++ SYNC_ANY_GITHUBS_NOT_ON_GITLAB.js
@@ -0,0 +1,106 @@
+const fetch = require('node-fetch')
+const FormData = require('form-data')
+const { gitHubToken, gitLabToken, user } = require('./.SECRETS')
+const gitHubApi = 'https://api.github.com'
+const gitLabApi = 'https://gitlab.com/api/v4'
+
+const sanitizeName = (name) => name.replace(/[^A-Za-z0-9]/g, '').toLowerCase()
+
+const getRepos = (obj) => {
+    obj.options = obj.options || {}
+    let repos = {}
+    const getSet = (count) =>
+        new Promise((resolve, reject) =>
+            fetch(obj.url(count), obj.options)
+                .then((res) => res.json())
+                .then((set) => {
+                    if (set.length) {
+                        console.log(`Page ${count} complete.`)
+                        set.forEach((repo) => {
+                            console.log(repo)
+                            repos[sanitizeName(repo.name)] = {
+                                default_branch: repo.default_branch,
+                                git_url: repo.git_url || null,
+                                name: repo.name,
+                                visibility: repo.private ? 'private' : 'public'
+                            }
+                            return repos
+                        })
+                        return resolve(getSet(++count))
+                    } else {
+                        return resolve(repos)
+                    }
+                })
+                .catch((err) => console.error(err))
+        )
+    return getSet(0)
+}
+
+// GitHub:
+getRepos({
+    url: (count) => `${gitHubApi}/users/${user}/repos?type=all&page=${count}`,
+    options: {
+        headers: {
+            Authorization: `token ${gitHubToken}`
+        }
+    }
+})
+    .then((gitHubRepos) => {
+        // GitLab:
+        getRepos({
+            url: (count) => `${gitLabApi}/users/${user}/projects?page=${count}`
+        })
+            .then((gitLabRepos) => {
+                // console.log('GitHub: ', gitHubRepos)
+                // console.log('GitLab: ', gitLabRepos)
+
+                const cleanGitHubRepos = Object.keys(gitHubRepos)
+                const cleanGitLabRepos = Object.keys(gitLabRepos)
+                const notOnGitLab = [
+                    ...new Set(
+                        cleanGitHubRepos.filter(
+                            (x) => !cleanGitLabRepos.includes(x)
+                        )
+                    )
+                ]
+                // console.log('Not on GitLab: ', notOnGitLab)
+
+                notOnGitLab.forEach((repo) => {
+                    const formData = new FormData()
+
+                    formData.append(
+                        'default_branch',
+                        `${gitHubRepos[repo].default_branch}`
+                    )
+                    formData.append(
+                        'import_url',
+                        `${gitHubRepos[repo].git_url}`
+                    )
+                    formData.append('mirror', 'true')
+                    formData.append('mirror_trigger_builds', 'true')
+                    formData.append(
+                        'mirror_overwrites_diverged_branches',
+                        'true'
+                    )
+                    formData.append('name', `${gitHubRepos[repo].name}`)
+                    formData.append('path', `${gitHubRepos[repo].name}`)
+                    formData.append(
+                        'visibility',
+                        `${gitHubRepos[repo].visibility}`
+                    )
+
+                    fetch(`${gitLabApi}/projects`, {
+                        method: 'POST',
+                        headers: {
+                            'Private-Token': gitLabToken
+                        },
+                        body: formData
+                    })
+                        .then((res) => res.json())
+                        .then((json) => console.log(json))
+                        .catch((err) => console.error(err))
+                })
+            })
+            .catch((err) => console.error(err))
+    })
+    .catch((err) => console.error(err))
